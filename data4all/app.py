eee4b6790399e0b52aa10c81f0ceebc22e241c93
YuviPanda <yuvipanda@gmail.com>
2014-07-21 02:44:33 +0530

    Add list all queries page

--- data4all/app.py
+++ data4all/app.py
@@ -30,5 +30,21 @@ def new_query():
     user = mwoauth.get_current_user()
     return render_template("query/new.html", user=user)
 
+
+@app.route("/query/all")
+def all_queries():
+    user = mwoauth.get_current_user()
+    queries = []
+    statuses = ['queued', 'running', 'killed', 'done']
+    owners = ['yuvipanda', 'halfak', 'J-Mo']
+    for i in xrange(25):
+        queries.append({
+            'id': i,
+            'title': 'Random looking query %d' % i,
+            'status': statuses[i % 3],
+            'owner': owners[i % 2]
+        })
+    return render_template("query/list.html", user=user, queries=queries)
+
 if __name__ == '__main__':
     app.run(port=6000, host="0.0.0.0")

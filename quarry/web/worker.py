99ff198d74eda31b742015833ace2dce56b171b3
YuviPanda <yuvipanda@gmail.com>
2014-08-03 06:54:54 +0530

    Use only one set of connections per worker process
    Verified that the connections are being closed during
    both ctrl-c shut down of the master worker and also
    when the tasks are revoked + terminated. Also expliclty
    kill the query on worker termination anyway
    
    Change-Id: I46c1f25f2b572518906f90062db4cd91d7732716

--- quarry/web/worker.py
+++ quarry/web/worker.py
@@ -6,6 +6,7 @@ from celery.utils.log import get_task_logger
 from sqlactions import check_sql
 from models.query import QueryRun
 from celery import Celery
+from celery.signals import worker_process_init, worker_process_shutdown
 from connections import Connections
 import time
 import yaml
@@ -24,6 +25,7 @@ TaskBase = celery.Task
 # FIXME: Find a way to not need this?
 # Without this, g.conn isn't set in a way that models can share
 fake_app = Flask('faaaaaakeeee')
+conn = None
 
 
 class ContextTask(TaskBase):
@@ -31,11 +33,8 @@ class ContextTask(TaskBase):
 
     def __call__(self, *args, **kwargs):
         with fake_app.app_context():
-            g.conn = Connections(celery.conf)
-            try:
-                return TaskBase.__call__(self, *args, **kwargs)
-            finally:
-                g.conn.close_all()
+            g.conn = conn
+            return TaskBase.__call__(self, *args, **kwargs)
 
 
 celery.Task = ContextTask
@@ -48,6 +47,21 @@ def make_result(cur):
         'headers': [c[0] for c in cur.description],
         'rows': cur.fetchall()
     }
+
+
+@worker_process_init.connect
+def init(sender, signal):
+    global conn
+    conn = Connections(celery.conf)
+    celery_log.info("Initialized lazy loaded connections")
+
+
+@worker_process_shutdown.connect
+def shutdown(sender, signal, pid, exitcode):
+    global conn
+    kill_query.delay(conn.replica.thread_id())
+    conn.close_all()
+    celery_log.info("Closed all connection")
 
 
 @celery.task(name='worker.kill_query')
@@ -68,37 +82,36 @@ def kill_query(thread_id):
 
 @celery.task(name='worker.run_query')
 def run_query(query_run_id):
-    qrun = QueryRun.get_by_id(query_run_id)
-    celery_log.info("Starting run for qrun:%s", qrun.id)
+    cur = False
     try:
+        celery_log.info("Starting run for qrun:%s", query_run_id)
         qrun = QueryRun.get_by_id(query_run_id)
         qrun.status = QueryRun.STATUS_RUNNING
         qrun.save()
-        try:
-            check_result = check_sql(qrun.query_rev.text)
-            start_time = time.clock()
-            cur = g.conn.replica.cursor()
-            if check_result is not True:
-                celery_log.info("Check result for qrun:%s failed, with message: %s", qrun.id, check_result[0])
-                raise pymysql.DatabaseError(0, check_result[1])
-            cur.execute(qrun.query_rev.text)
-            result = []
+        check_result = check_sql(qrun.query_rev.text)
+        start_time = time.clock()
+        if check_result is not True:
+            celery_log.info("Check result for qrun:%s failed, with message: %s", qrun.id, check_result[0])
+            raise pymysql.DatabaseError(0, check_result[1])
+        cur = g.conn.replica.cursor()
+        cur.execute(qrun.query_rev.text)
+        result = []
+        result.append(make_result(cur))
+        while cur.nextset():
             result.append(make_result(cur))
-            while cur.nextset():
-                result.append(make_result(cur))
-            total_time = time.clock() - start_time
-            qresult = QuerySuccessResult(qrun, total_time, result, celery.conf.OUTPUT_PATH_TEMPLATE)
-            qrun.status = QueryRun.STATUS_COMPLETE
-            celery_log.info("Completed run for qrun:%s successfully", qrun.id)
-        except pymysql.DatabaseError as e:
-            total_time = time.clock() - start_time
-            qresult = QueryErrorResult(qrun, total_time, celery.conf.OUTPUT_PATH_TEMPLATE, e.args[1])
-            qrun.status = QueryRun.STATUS_FAILED
-            celery_log.info("Completed run for qrun:%s with failure: %s", qrun.id, e.args[1])
-        finally:
-            cur.close()
+        total_time = time.clock() - start_time
+        qresult = QuerySuccessResult(qrun, total_time, result, celery.conf.OUTPUT_PATH_TEMPLATE)
+        qrun.status = QueryRun.STATUS_COMPLETE
+        celery_log.info("Completed run for qrun:%s successfully", qrun.id)
         qresult.output()
         qrun.save()
+    except pymysql.DatabaseError as e:
+        total_time = time.clock() - start_time
+        qresult = QueryErrorResult(qrun, total_time, celery.conf.OUTPUT_PATH_TEMPLATE, e.args[1])
+        qrun.status = QueryRun.STATUS_FAILED
+        qresult.output()
+        qrun.save()
+        celery_log.info("Completed run for qrun:%s with failure: %s", qrun.id, e.args[1])
     except SoftTimeLimitExceeded:
         celery_log.info(
             "Time limit exceeded for qrun:%s, thread:%s attempting to kill",
@@ -110,3 +123,8 @@ def run_query(query_run_id):
         qrun.save()
         qresult = QueryKilledResult(qrun, total_time, celery.conf.OUTPUT_PATH_TEMPLATE)
         qresult.output()
+    finally:
+        if cur is not False:
+            # It is possible the cursor was never created,
+            # so check before we try to close it
+            cur.close()

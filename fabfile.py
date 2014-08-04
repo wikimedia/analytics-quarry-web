86fbd4fe11f0e7d04bb4b764af7d8e0508c249fb
YuviPanda <yuvipanda@gmail.com>
2014-08-04 05:38:37 +0530

    Add fabfile
    Change-Id: I51c8051d589925b6b7e32ed0b2d39b9db65fddb8

---
+++ fabfile.py
@@ -0,0 +1,44 @@
+from fabric.api import sudo, env, cd, put, roles
+
+env.roledefs = {
+    'web': ['quarry-web-test.eqiad.wmflabs'],
+    'db': ['quarry-web-test.eqiad.wmflabs'],
+    'runner': ['quarry-runner-test.eqiad.wmflabs']
+}
+env.use_ssh_config = True
+
+venv_dir = '/srv/venv'
+code_dir = '/srv/quarry'
+
+
+def sr(cmd):
+    return sudo(cmd, user='quarry')
+
+
+@roles('web', 'runner')
+def update_git():
+    with cd(code_dir):
+        sr('git fetch origin')
+        sr('git reset --hard origin/master')
+
+
+@roles('web')
+def restart_uwsgi():
+    sudo('service uwsgi restart')
+
+
+@roles('runner')
+def restart_celery():
+    sudo('service celeryd restart')
+
+
+@roles('web', 'runner')
+def update_config(config_file):
+    put(config_file, '/srv/quarry/quarry/config.yaml', use_sudo=True)
+    sudo('chown quarry:www-data /srv/quarry/quarry/config.py')
+
+
+@roles('db')
+def run_sql(sql_file):
+    put(sql_file, '/tmp/%s' % sql_file)
+    sr('mysql -u root -p < "/tmp/%s"' % sql_file)

195f3683a595441f47b72edae276649dc439457e
YuviPanda <yuvipanda@gmail.com>
2014-08-26 23:46:36 +0100

    Added CSV and TSV output format generation
    Change-Id: I80228191b266d8ec6a6d498a5079a1f9ba78ce26

--- quarry/web/output.py
+++ quarry/web/output.py
@@ -1,10 +1,36 @@
 from flask import Response
 import json
+import unicodecsv
 
 
 def get_formatted_response(format, reader, resultset_id):
     if format == 'json':
         return json_formatter(reader, resultset_id)
+    elif format == 'csv':
+        return separated_formatter(reader, resultset_id, ',')
+    elif format == 'tsv':
+        return separated_formatter(reader, resultset_id, "\t")
+
+
+class OneLineRetainer(object):
+    def __init__(self):
+        self.last_written = None
+
+    def write(self, value):
+        self.last_written = value
+
+
+def separated_formatter(reader, resultset_id, delim=','):
+    rows = reader.get_rows(resultset_id)
+    retainer = OneLineRetainer()
+    writer = unicodecsv.writer(retainer, delimiter=delim)
+
+    def respond():
+        for row in rows:
+            writer.writerow(row)
+            yield retainer.last_written
+
+    return Response(respond(), content_type='text/csv')
 
 
 def json_formatter(reader, resultset_id):

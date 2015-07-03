e0dee0b722a2c6e05fb8e9d02d2bc1066e8f3b18
YuviPanda <yuvipanda@gmail.com>
2015-07-03 22:34:03 +0100

    Make tables.sql idempotent and fix stupid extra ,
    Change-Id: I0db234033d4b1f54428fc3ac338b48cd74270d19

--- tables.sql
+++ tables.sql
@@ -1,23 +1,23 @@
 CREATE DATABASE IF NOT EXISTS quarry CHARACTER SET utf8;
 USE quarry;
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) BINARY NOT NULL UNIQUE,
     wiki_uid INT UNSIGNED NOT NULL UNIQUE
 );
-CREATE UNIQUE INDEX user_username_index ON user(username);
-CREATE UNIQUE INDEX user_wiki_uid ON user(wiki_uid);
+CREATE UNIQUE INDEX IF NOT EXISTS user_username_index ON user( username);
+CREATE UNIQUE INDEX IF NOT EXISTS user_wiki_uid ON user(wiki_uid);
 
-CREATE TABLE user_group(
+CREATE TABLE IF NOT EXISTS user_group(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     group_name VARCHAR(255) BINARY NOT NULL
 );
-CREATE INDEX user_group_user_group_index ON user_group(user_id, group_name);
-CREATE INDEX user_group_user_id_index ON user_group(user_id);
-CREATE INDEX user_group_group_name_index ON user_group(group_name);
+CREATE INDEX IF NOT EXISTS user_group_user_group_index ON user_group(user_id, group_name);
+CREATE INDEX IF NOT EXISTS user_group_user_id_index ON user_group(user_id);
+CREATE INDEX IF NOT EXISTS user_group_group_name_index ON user_group(group_name);
 
-CREATE TABLE query(
+CREATE TABLE IF NOT EXISTS query(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     title VARCHAR(1024) BINARY,
@@ -27,32 +27,32 @@ CREATE TABLE query(
     description TEXT BINARY,
     parent_id INT UNSIGNED
 );
-CREATE INDEX query_parent_id_index ON query(parent_id);
+CREATE INDEX IF NOT EXISTS query_parent_id_index ON query(parent_id);
 
-CREATE TABLE query_revision(
+CREATE TABLE IF NOT EXISTS query_revision(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     text TEXT BINARY NOT NULL,
     query_id INT UNSIGNED NOT NULL,
     latest_run_id INT UNSIGNED,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX query_rev_query_id_index ON query_revision(query_id);
+CREATE INDEX IF NOT EXISTS query_rev_query_id_index ON query_revision(query_id);
 
-CREATE TABLE query_run(
+CREATE TABLE IF NOT EXISTS query_run(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     query_rev_id INT UNSIGNED NOT NULL,
     status TINYINT UNSIGNED NOT NULL DEFAULT 0,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     task_id VARCHAR(36) BINARY,
-    extra_info TEXT BINARY,
+    extra_info TEXT BINARY
 );
-CREATE INDEX query_run_status_index ON query_run(status);
+CREATE INDEX IF NOT EXISTS query_run_status_index ON query_run(status);
 
-CREATE TABLE star(
+CREATE TABLE IF NOT EXISTS star(
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     user_id INT UNSIGNED NOT NULL,
     query_id INT UNSIGNED NOT NULL,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-CREATE INDEX star_user_id_index ON star(user_id);
-CREATE INDEX star_query_id_index ON star(query_id);
+CREATE INDEX IF NOT EXISTS star_user_id_index ON star(user_id);
+CREATE INDEX IF NOT EXISTS star_query_id_index ON star(query_id);

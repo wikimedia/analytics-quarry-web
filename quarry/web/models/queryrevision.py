ca82715be386e24782f412f64e403d6d539d27b6
Alex Monk <krenair@gmail.com>
2016-01-27 22:22:19 +0000

    Database selection
    TODO: This will probably cause issues with line numbers because of the concatenation.
    
    Bug: T76466
    Change-Id: If2894324830abdf03d0a8a3ef8e60ec6f64ca304

--- quarry/web/models/queryrevision.py
+++ quarry/web/models/queryrevision.py
@@ -9,11 +9,16 @@ class QueryRevision(Base):
 
     id = Column(Integer, primary_key=True)
     text = Column(Unicode(4096))
+    dbs = Column(Unicode(16384))
     query_id = Column(Integer, ForeignKey('query.id'))
     timestamp = Column(DateTime)
     latest_run_id = Column(Integer, ForeignKey('query_run.id'))
 
     latest_run = relationship('QueryRun', primaryjoin='QueryRevision.latest_run_id == QueryRun.id', uselist=False)
+
+    # According to springle hitting this db can fuck things up for everyone, and it
+    # isn't easy to restrict access to this from mysql. All values must be lowercase.
+    BAD_DATABASES = ['information_schema']
 
     def is_allowed(self):
         """Check if given SQL is ok to execute.
@@ -24,16 +29,23 @@ class QueryRevision(Base):
 
         returns tuple of (actual_reason, public_reason_string)
         """
-        if 'information_schema' in self.text.lower():
-            # According to springle hitting this db can fuck
-            # things up for everyone, and it isn't easy to
-            # restrict access to this from mysql
-            return ("Hitting information_schema", "Unauthorized access to restricted database")
+
+        for db in self.get_dbs():
+            if db.lower() in QueryRevision.BAD_DATABASES:
+                return ("Hitting " + db, "Unauthorized access to restricted database")
+
+        for bad_db in QueryRevision.BAD_DATABASES:
+            if bad_db in self.text.lower():
+                return ("Hitting " + bad_db, "Unauthorized access to restricted database")
         return True
+
+    def get_dbs(self):
+        return self.dbs.split(';')
 
     def to_json(self):
         return {
             'id': self.id,
+            'dbs': self.get_dbs(),
             'sql': self.text,
             'timestamp': self.timestamp,
             'latest_run': self.latest_run.id,

79e1b0cd48df1ff2d81dd297acddae6620f7e932
zhuyifei1999 <zhuyifei1999@gmail.com>
2018-09-16 13:31:38 -0500

    Add ESLint
    JS code is such a mess here :(
    
    Run via `npm test`.
    
    Change-Id: I67587b04c9fb724aeb0f761be6ee7b009f7525fd

--- quarry/web/static/js/query/view.js
+++ quarry/web/static/js/query/view.js
@@ -1,192 +1,193 @@
-$( function() {
-    function htmlEscape(str) {
-        return String(str)
-                .replace(/&/g, '&amp;')
-                .replace(/"/g, '&quot;')
-                .replace(/'/g, '&#39;')
-                .replace(/</g, '&lt;')
-                .replace(/>/g, '&gt;');
-    }
+$( function () {
+	function htmlEscape( str ) {
+		return String( str )
+			.replace( /&/g, '&amp;' )
+			.replace( /"/g, '&quot;' )
+			.replace( /'/g, '&#39;' )
+			.replace( /</g, '&lt;' )
+			.replace( />/g, '&gt;' );
+	}
 
-    CodeMirror.extendMode( 'sql', { electricChars: ')' } );
-    var editor = CodeMirror.fromTextArea( $("#code")[0], {
-        mode: "text/x-mariadb",
-        theme: "monokai",
-        readOnly: !vars.can_edit,
-        matchBrackets: true
-    });
+	CodeMirror.extendMode( 'sql', { electricChars: ')' } );
+	var editor = CodeMirror.fromTextArea( $( '#code' )[ 0 ], {
+		mode: 'text/x-mariadb',
+		theme: 'monokai',
+		readOnly: !vars.can_edit,
+		matchBrackets: true
+	} );
 
-    $('#query-description').autosize();
+	$( '#query-description' ).autosize();
 
-    if (vars.can_edit) {
-        $('#title').blur(function() {
-            $.post( "/api/query/meta", {
-                query_id: vars.query_id,
-                title: $('#title').val()
-            } ).done( function( data ) {
-                // Uh, do nothing
-            } );
-        });
-    }
+	if ( vars.can_edit ) {
+		$( '#title' ).blur( function () {
+			$.post( '/api/query/meta', {
+				query_id: vars.query_id,
+				title: $( '#title' ).val()
+			} ).done( function ( /* data */ ) {
+				// Uh, do nothing
+			} );
+		} );
+	}
 
-    $("#un-star-query").click( function() {
-        $.post( "/api/query/unstar", {
-            query_id: vars.query_id
-        }).done(function( data ) {
-            $('#content').removeClass('starred');
-        });
-    });
+	$( '#un-star-query' ).click( function () {
+		$.post( '/api/query/unstar', {
+			query_id: vars.query_id
+		} ).done( function ( /* data */ ) {
+			$( '#content' ).removeClass( 'starred' );
+		} );
+	} );
 
-    $("#star-query").click( function() {
-        $.post( "/api/query/star", {
-            query_id: vars.query_id
-        }).done(function( data ) {
-            $('#content').addClass('starred');
-        });
-    });
+	$( '#star-query' ).click( function () {
+		$.post( '/api/query/star', {
+			query_id: vars.query_id
+		} ).done( function ( /* data */ ) {
+			$( '#content' ).addClass( 'starred' );
+		} );
+	} );
 
-    $("#query-description").blur( function() {
-        $.post( "/api/query/meta", {
-            query_id: vars.query_id,
-            description: $("#query-description").val()
-        } ).done( function() {
-            // Uh, do nothing?
-        } );
-    } );
+	$( '#query-description' ).blur( function () {
+		$.post( '/api/query/meta', {
+			query_id: vars.query_id,
+			description: $( '#query-description' ).val()
+		} ).done( function () {
+			// Uh, do nothing?
+		} );
+	} );
 
-    $("#toggle-publish").click( function() {
-        $.post( "/api/query/meta", {
-            query_id: vars.query_id,
-            published: vars.published ? 0 : 1
-        }).done(function( data ) {
-            $("#content").toggleClass("published");
-            vars.published = !vars.published;
-        } );
-    } );
+	$( '#toggle-publish' ).click( function () {
+		$.post( '/api/query/meta', {
+			query_id: vars.query_id,
+			published: vars.published ? 0 : 1
+		} ).done( function ( /* data */ ) {
+			$( '#content' ).toggleClass( 'published' );
+			vars.published = !vars.published;
+		} );
+	} );
 
-    $('#run-code').click( function() {
-        $.post( "/api/query/run", {
-            text: editor.getValue(),
-            query_id: vars.query_id
-        }).done( function( data ) {
-            var d = JSON.parse(data);
-            vars.output_url = d.output_url;
-            $("#query-progress").show();
-            $("#query-result-error").hide();
-            $("#query-result-success").hide();
-            clearTimeout( window.lastStatusCheck );
-            checkStatus( d.qrun_id, false );
-        } );
+	$( '#run-code' ).click( function () {
+		$.post( '/api/query/run', {
+			text: editor.getValue(),
+			query_id: vars.query_id
+		} ).done( function ( data ) {
+			var d = JSON.parse( data );
+			vars.output_url = d.output_url;
+			$( '#query-progress' ).show();
+			$( '#query-result-error' ).hide();
+			$( '#query-result-success' ).hide();
+			clearTimeout( window.lastStatusCheck );
+			checkStatus( d.qrun_id, false );
+		} );
 
-        return false;
-    } );
+		return false;
+	} );
 
-    function checkStatus( qrun_id, silent ) {
-        var url = '/run/' + qrun_id + '/status';
-        $.get( url ).done( function( data ) {
-            $( "#query-status" ).html( 'Query status: <strong>' + data.status + '</strong>' );
-            $('#query-result').html(
-                nunjucks.render( 'query-status.html', data )
-            );
-            if ( data.status === 'complete' ) {
-                // kick off other things!
-                populateResults( qrun_id, 0, data.extra.resultsets.length );
-                if ( !silent && vars.preferences[ 'use_notifications' ] ) {
-                    var title = $( '#title' ).val() ? '"' + $( '#title' ).val() + '"' : 'Untitled query #' + vars.query_id;
-                    sendNotification( title + ' execution has been completed' );
-                }
-            } else if ( data.status === 'queued' || data.status === 'running' ) {
-                window.lastStatusCheck = setTimeout( function() { checkStatus( qrun_id, false ); }, 5000 );
-            }
+	function checkStatus( qrun_id, silent ) {
+		var url = '/run/' + qrun_id + '/status';
+		$.get( url ).done( function ( data ) {
+			$( '#query-status' ).html( 'Query status: <strong>' + data.status + '</strong>' );
+			$( '#query-result' ).html(
+				nunjucks.render( 'query-status.html', data )
+			);
+			if ( data.status === 'complete' ) {
+				// kick off other things!
+				populateResults( qrun_id, 0, data.extra.resultsets.length );
+				if ( !silent && vars.preferences.use_notifications ) {
+					var title = $( '#title' ).val() ? '"' + $( '#title' ).val() + '"' : 'Untitled query #' + vars.query_id;
+					sendNotification( title + ' execution has been completed' );
+				}
+			} else if ( data.status === 'queued' || data.status === 'running' ) {
+				window.lastStatusCheck = setTimeout( function () { checkStatus( qrun_id, false ); }, 5000 );
+			}
 
-            $( '#show-explain' ).off().click( function () {
-                $.get( '/explain/' + data.extra.connection_id ).done( function( data ) {
-                    var $table = $( '#explain-results-table' );
-                    if ( !$table.length ) {
-                        var $table = $( '<table>' ).attr( {
-                            class: 'table',
-                            id: 'explain-results-table',
-                        } );
+			$( '#show-explain' ).off().click( function () {
+				$.get( '/explain/' + data.extra.connection_id ).done( function ( data ) {
+					var $table = $( '#explain-results-table' );
+					if ( !$table.length ) {
+						$table = $( '<table>' ).attr( {
+							'class': 'table',
+							id: 'explain-results-table'
+						} );
 
-                        $( '#query-result-container' ).append( $table );
-                    }
+						$( '#query-result-container' ).append( $table );
+					}
 
-                    populateTable( $table, data );
-                } );
-            } );
-        } );
-    }
+					populateTable( $table, data );
+				} );
+			} );
+		} );
+	}
 
-    function populateTable( $table, data ) {
-        var columns = [];
-        $.each( data.headers, function( i, header ) {
-            columns.push( {
-                'title': htmlEscape( header ),
-                'render': function( data, type, row ) {
-                    if ( typeof data === 'string' ) {
-                        return htmlEscape( data );
-                    } else {
-                        return data;
-                    }
-                }
-            } );
-        } );
+	function populateTable( $table, data ) {
+		var columns = [];
+		$.each( data.headers, function ( i, header ) {
+			columns.push( {
+				title: htmlEscape( header ),
+				render: function ( data /* , type, row */ ) {
+					if ( typeof data === 'string' ) {
+						return htmlEscape( data );
+					} else {
+						return data;
+					}
+				}
+			} );
+		} );
 
-        $table.dataTable({
-            'data': data.rows,
-            'columns': columns,
-            'scrollX': true,
-            'pagingType': 'simple_numbers',
-            'paging': data.rows.length > 100,
-            'pageLength': 100,
-            'deferRender': true,
-            'order': [],
-            'destroy': true,
-        } );
+		$table.dataTable( {
+			data: data.rows,
+			columns: columns,
+			scrollX: true,
+			pagingType: 'simple_numbers',
+			paging: data.rows.length > 100,
+			pageLength: 100,
+			deferRender: true,
+			order: [],
+			destroy: true
+		} );
 
-        // Ugly hack to ensure that table rows actually show
-        // up. Otherwise they don't until you do a resize.
-        // Browser and DOM bugs are the best.
-        $table.DataTable().draw();
-    }
+		// Ugly hack to ensure that table rows actually show
+		// up. Otherwise they don't until you do a resize.
+		// Browser and DOM bugs are the best.
+		$table.DataTable().draw();
+	}
 
-    function populateResults(qrun_id, resultset_id, till) {
-        var url = '/run/' + qrun_id + '/output/' + resultset_id + '/json';
-        console.log( url );
-        $.get( url ).done( function( data ) {
-            var tableContainer = $( nunjucks.render( 'query-resultset.html', {
-                'only_resultset': resultset_id === till - 1,
-                'resultset_number': resultset_id + 1,
-                'rowcount': data.rows.length,
-                'resultset_id': resultset_id,
-                'run_id': qrun_id
-            } ) );
-            var $table = tableContainer.find( 'table' );
-            $( '#query-result' ).append( tableContainer );
+	function populateResults( qrun_id, resultset_id, till ) {
+		var url = '/run/' + qrun_id + '/output/' + resultset_id + '/json';
+		console.log( url );
+		$.get( url ).done( function ( data ) {
+			var tableContainer = $( nunjucks.render( 'query-resultset.html', {
+					only_resultset: resultset_id === till - 1,
+					resultset_number: resultset_id + 1,
+					rowcount: data.rows.length,
+					resultset_id: resultset_id,
+					run_id: qrun_id
+				} ) ),
+				$table = tableContainer.find( 'table' );
+			$( '#query-result' ).append( tableContainer );
 
-            populateTable( $table, data );
+			populateTable( $table, data );
 
-            if ( resultset_id < till - 1 ) {
-                populateResults( qrun_id, resultset_id + 1, till );
-            }
-        } );
-    }
+			if ( resultset_id < till - 1 ) {
+				populateResults( qrun_id, resultset_id + 1, till );
+			}
+		} );
+	}
 
-    function sendNotification( text ) {
-        if ( Notification.permission === 'granted' ) {
-            var notification = new Notification( 'Quarry', {
-                icon: '/static/img/quarry-logo-icon.svg',
-                body: text,
-            } );
-        } else {
-            console.log( "Can't send notification, permission value is set to " + Notification.permission );
-            $.get( '/api/preferences/set/use_notifications/null' );
-        }
-    }
+	function sendNotification( text ) {
+		if ( Notification.permission === 'granted' ) {
+			// eslint-disable-next-line no-new
+			new Notification( 'Quarry', {
+				icon: '/static/img/quarry-logo-icon.svg',
+				body: text
+			} );
+		} else {
+			console.log( 'Can\'t send notification, permission value is set to ' + Notification.permission );
+			$.get( '/api/preferences/set/use_notifications/null' );
+		}
+	}
 
-    if ( vars.qrun_id ) {
-        checkStatus( vars.qrun_id, true );
-    } else {
-        $( '#query-status' ).text( 'This query has never yet been executed' );
-    }
+	if ( vars.qrun_id ) {
+		checkStatus( vars.qrun_id, true );
+	} else {
+		$( '#query-status' ).text( 'This query has never yet been executed' );
+	}
 } );
--- quarry/web/static/js/user.js
+++ quarry/web/static/js/user.js
@@ -1,32 +1,33 @@
-$( function() {
-    if ( $( "#option-useNotifications" ).length ) {
-        if ( !window.Notification ) {
-            $( "#option-useNotifications .error" ).css( 'display', 'inline' );
-            $( "#option-useNotifications-checkbox" ).prop( 'disabled', true );
-        }
+$( function () {
+	if ( $( '#option-useNotifications' ).length ) {
+		if ( !window.Notification ) {
+			$( '#option-useNotifications .error' ).css( 'display', 'inline' );
+			$( '#option-useNotifications-checkbox' ).prop( 'disabled', true );
+		}
 
-        $( "#option-useNotifications-checkbox" ).prop( "checked", vars.preferences[ 'use_notifications' ] );
+		$( '#option-useNotifications-checkbox' ).prop( 'checked', vars.preferences.use_notifications );
 
-        $( "#option-useNotifications-checkbox" ).change( function () {
-            if ( this.checked ) {
-                Notification.requestPermission().then( function( result ) {
-                    if ( result === 'granted' ) {
-                        var notification = new Notification( 'Quarry', {
-                            icon: '/static/img/quarry-logo-icon.svg',
-                            body: 'Test notification!',
-                        } );
-                        $.get( '/api/preferences/set/use_notifications/1' );
-                    } else if ( result === 'denied' ) {
-                        alert( "Can't get notifications permission." );
-                        this.checked = false;
-                    } else {
-                        alert( 'You need to allow notifications for this website to enable this option.' );
-                        this.checked = false;
-                    }
-                } );
-            } else {
-                $.get( '/api/preferences/set/use_notifications/null' );
-            }
-        } );
-    }
+		$( '#option-useNotifications-checkbox' ).change( function () {
+			if ( this.checked ) {
+				Notification.requestPermission().then( function ( result ) {
+					if ( result === 'granted' ) {
+						// eslint-disable-next-line no-new
+						new Notification( 'Quarry', {
+							icon: '/static/img/quarry-logo-icon.svg',
+							body: 'Test notification!'
+						} );
+						$.get( '/api/preferences/set/use_notifications/1' );
+					} else if ( result === 'denied' ) {
+						alert( 'Can\'t get notifications permission.' );
+						this.checked = false;
+					} else {
+						alert( 'You need to allow notifications for this website to enable this option.' );
+						this.checked = false;
+					}
+				} );
+			} else {
+				$.get( '/api/preferences/set/use_notifications/null' );
+			}
+		} );
+	}
 } );

4b2d86253b2fde19e60a6b22ead5f48400f9a7e3
Framawiki <framawiki@tools.wmflabs.org>
2018-09-16 13:08:34 +0200

    output.py: handle bad file format
    Change-Id: I9831524b94918b57393e56547469fb4bd54e8695

--- quarry/web/output.py
+++ quarry/web/output.py
@@ -22,6 +22,7 @@ def get_formatted_response(format, queryrun, reader, resultset_id):
         return xlsx_formatter(reader, resultset_id)
     elif format == 'html':
         return html_formatter(reader, resultset_id)
+    return Response('Bad file format', status=400)
 
 
 class MultipleLinesRetainer(object):  # TODO: generator ?

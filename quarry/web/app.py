2a1e2bd43097f8ed1b32fcd018177aedb13099f1
Andrew Bogott <abogott@wikimedia.org>
2021-08-30 17:04:36 -0500

    app.py: process with black in anticipation of a huge reformat
    Change-Id: I5855d4f7dae2a0e3757a8d9c83c27787aee1a72c

--- quarry/web/app.py
+++ quarry/web/app.py
@@ -49,14 +49,14 @@ app.session_interface = RedisSessionInterface(global_conn.redis)
 class QueriesRangeBasedPagination(RangeBasedPagination):
     def get_page_link(self, page_key, limit):
         get_params = dict(request.args)
-        get_params.update({
-            'from': page_key, 'limit': limit})
-        return url_for('query_runs_all', **dict(
-            [(key, value) for key, value in list(get_params.items())])
+        get_params.update({"from": page_key, "limit": limit})
+        return url_for(
+            "query_runs_all",
+            **dict([(key, value) for key, value in list(get_params.items())])
         )
 
     def order_queryset(self):
-        if self.direction == 'next':
+        if self.direction == "next":
             self.queryset = self.queryset.order_by(desc(QueryRun.timestamp))
         else:
             self.queryset = self.queryset.order_by(QueryRun.timestamp)
@@ -67,12 +67,10 @@ class QueriesRangeBasedPagination(RangeBasedPagination):
         from_query = g.conn.session.query(Query).get(self.page_key)
         if from_query:
             from_qrun_id = from_query.latest_rev.latest_run.id
-            if self.direction == 'prev':
-                self.queryset = self.queryset.filter(
-                    QueryRun.id > from_qrun_id)
+            if self.direction == "prev":
+                self.queryset = self.queryset.filter(QueryRun.id > from_qrun_id)
             else:
-                self.queryset = self.queryset.filter(
-                    QueryRun.id < from_qrun_id)
+                self.queryset = self.queryset.filter(QueryRun.id < from_qrun_id)
 
 
 @app.before_request
@@ -96,12 +94,14 @@ def index():
 def unstar_query():
     if get_user() is None:
         return "Unauthorized access", 403
-    query = g.conn.session.query(Query).get(request.form['query_id'])
+    query = g.conn.session.query(Query).get(request.form["query_id"])
     if query:
-        star = g.conn.session.query(Star)\
-            .filter(Star.query_id == request.form['query_id'])\
-            .filter(Star.user_id == get_user().id)\
+        star = (
+            g.conn.session.query(Star)
+            .filter(Star.query_id == request.form["query_id"])
+            .filter(Star.user_id == get_user().id)
             .one()
+        )
         g.conn.session.delete(star)
         g.conn.session.commit()
         return ""
@@ -113,7 +113,7 @@ def unstar_query():
 def star_query():
     if get_user() is None:
         return "Unauthorized access", 403
-    query = g.conn.session.query(Query).get(request.form['query_id'])
+    query = g.conn.session.query(Query).get(request.form["query_id"])
     if query:
         star = Star()
         star.user = get_user()
@@ -139,7 +139,7 @@ def new_query():
     query.user = get_user()
     g.conn.session.add(query)
     g.conn.session.commit()
-    return redirect(url_for('query_show', query_id=query.id))
+    return redirect(url_for("query_show", query_id=query.id))
 
 
 @app.route("/fork/<int:id>")
@@ -158,13 +158,13 @@ def fork_query(id):
     query_rev = QueryRevision(
         query_id=query.id,
         query_database=parent_query.latest_rev.query_database,
-        text=parent_query.latest_rev.text
-        )
+        text=parent_query.latest_rev.text,
+    )
     query.latest_rev = query_rev
     g.conn.session.add(query)
     g.conn.session.add(query_rev)
     g.conn.session.commit()
-    return redirect(url_for('query_show', query_id=query.id))
+    return redirect(url_for("query_show", query_id=query.id))
 
 
 @app.route("/query/<int:query_id>")
@@ -173,78 +173,95 @@ def query_show(query_id):
     can_edit = get_user() is not None and get_user().id == query.user_id
     is_starred = False
     if get_user():
-        is_starred = g.conn.session.query(func.count(Star.id))\
-            .filter(Star.user_id == get_user().id)\
-            .filter(Star.query_id == query_id).scalar() == 1
+        is_starred = (
+            g.conn.session.query(func.count(Star.id))
+            .filter(Star.user_id == get_user().id)
+            .filter(Star.query_id == query_id)
+            .scalar()
+            == 1
+        )
     jsvars = {
-        'query_id': query.id,
-        'can_edit': can_edit,
-        'is_starred': is_starred,
-        'published': query.published,
-        'preferences': get_preferences()
+        "query_id": query.id,
+        "can_edit": can_edit,
+        "is_starred": is_starred,
+        "published": query.published,
+        "preferences": get_preferences(),
     }
 
     if query.latest_rev and query.latest_rev.latest_run_id:
-        jsvars['qrun_id'] = query.latest_rev.latest_run_id
+        jsvars["qrun_id"] = query.latest_rev.latest_run_id
 
     return render_template(
         "query/view.html",
         user=get_user(),
         query=query,
         jsvars=jsvars,
-        latest_rev=query.latest_rev
+        latest_rev=query.latest_rev,
     )
 
 
-@app.route('/query/<int:query_id>/result/latest/<string:resultset_id>/<string:format>')
+@app.route("/query/<int:query_id>/result/latest/<string:resultset_id>/<string:format>")
 def query_output_redirect(query_id, resultset_id, format):
     query = g.conn.session.query(Query).filter(Query.id == query_id).one()
     qrun_id = query.latest_rev.latest_run_id
     # FIXME: Enforce HTTPS everywhere in a nicer way!
     resp = redirect(
-        url_for('output_result', qrun_id=qrun_id,
-                resultset_id=resultset_id, format=format,
-                _external=True, _scheme='https')
+        url_for(
+            "output_result",
+            qrun_id=qrun_id,
+            resultset_id=resultset_id,
+            format=format,
+            _external=True,
+            _scheme="https",
+        )
     )
     # CORS on the redirect
-    resp.headers.add('Access-Control-Allow-Origin', '*')
+    resp.headers.add("Access-Control-Allow-Origin", "*")
     return resp
 
 
-@app.route('/api/query/meta', methods=['POST'])
+@app.route("/api/query/meta", methods=["POST"])
 def api_set_meta():
     if get_user() is None:
         return "Authentication required", 401
 
-    query = g.conn.session.query(Query).filter(Query.id == request.form['query_id']).one()
+    query = (
+        g.conn.session.query(Query).filter(Query.id == request.form["query_id"]).one()
+    )
 
     if query.user_id != get_user().id:
         return "Authorization denied", 403
 
-    if 'title' in request.form:
-        query.title = request.form['title']
-    if 'published' in request.form:
-        query.published = request.form['published'] == '1'
-    if 'description' in request.form:
-        query.description = request.form['description']
+    if "title" in request.form:
+        query.title = request.form["title"]
+    if "published" in request.form:
+        query.published = request.form["published"] == "1"
+    if "description" in request.form:
+        query.description = request.form["description"]
     g.conn.session.add(query)
     g.conn.session.commit()
-    return json.dumps({'id': query.id})
+    return json.dumps({"id": query.id})
 
 
-@app.route('/api/query/run', methods=['POST'])
+@app.route("/api/query/run", methods=["POST"])
 def api_run_query():
     if get_user() is None:
         return "Authentication required", 401
-    text = request.form['text']
-    query_database = request.form['query_database'].lower().replace(" ", "")
-    query = g.conn.session.query(Query).filter(Query.id == request.form['query_id']).one()
+    text = request.form["text"]
+    query_database = request.form["query_database"].lower().replace(" ", "")
+    query = (
+        g.conn.session.query(Query).filter(Query.id == request.form["query_id"]).one()
+    )
     if not valid_dbname(query_database):
         return "Bad database name", 400
 
-    if query.user_id != get_user().id or \
-            g.conn.session.query(UserGroup).filter(UserGroup.user_id == get_user().id) \
-            .filter(UserGroup.group_name == 'blocked').first():
+    if (
+        query.user_id != get_user().id
+        or g.conn.session.query(UserGroup)
+        .filter(UserGroup.user_id == get_user().id)
+        .filter(UserGroup.group_name == "blocked")
+        .first()
+    ):
         return "Authorization denied", 403
 
     # Determine if already run, to update status in case job was killed
@@ -256,7 +273,9 @@ def api_run_query():
             g.conn.session.add(query.latest_rev.latest_run)
             g.conn.session.commit()
 
-    query_rev = QueryRevision(query_id=query.id, query_database=query_database, text=text)
+    query_rev = QueryRevision(
+        query_id=query.id, query_database=query_database, text=text
+    )
     query.latest_rev = query_rev
 
     # XXX (phuedx, 2014/08/08): This deviates from the pre-existing
@@ -274,84 +293,96 @@ def api_run_query():
     g.conn.session.add(query_rev)
     g.conn.session.add(query_run)
     g.conn.session.commit()
-    return json.dumps({
-        'qrun_id': query_run.id
-    })
+    return json.dumps({"qrun_id": query_run.id})
 
 
-@app.route('/api/query/stop', methods=['POST'])
+@app.route("/api/query/stop", methods=["POST"])
 def api_stop_query():
     if get_user() is None:
         return "Authentication required", 401
 
-    qrun_id = request.form['qrun_id']
-    db_of_process = request.form['query_database']
+    qrun_id = request.form["qrun_id"]
+    db_of_process = request.form["query_database"]
 
-# the db process id of the running job is stored in the query_run table while
-# the job is running. We can take this pid over to the database running the
-# query to stop the job
+    # the db process id of the running job is stored in the query_run table while
+    # the job is running. We can take this pid over to the database running the
+    # query to stop the job
     query_run = g.conn.session.query(QueryRun).filter(QueryRun.id == qrun_id)
     result_dictionary = ast.literal_eval(query_run[0].extra_info)
     if "connection_id" in result_dictionary:
         g.replica.connection = db_of_process
         cur = g.replica.connection.cursor()
-        cur.execute('KILL %s;', (result_dictionary["connection_id"]),)
+        cur.execute("KILL %s;", (result_dictionary["connection_id"]))
         output = "job stopped"
     else:
         output = "job not running"
 
-    return json.dumps({
-        'stopped': output
-    })
+    return json.dumps({"stopped": output})
 
 
 @app.route("/query/runs/all")
 def query_runs_all():
-    queries = g.conn.session.query(Query)\
-        .join(Query.latest_rev).join(QueryRevision.latest_run)
-    queries_filter = 'all'
-    if request.args.get('published') == 'true':
+    queries = (
+        g.conn.session.query(Query)
+        .join(Query.latest_rev)
+        .join(QueryRevision.latest_run)
+    )
+    queries_filter = "all"
+    if request.args.get("published") == "true":
         queries = queries.filter(Query.published)
-        queries_filter = 'published'
-    limit = int(request.args.get(
-        'limit', app.config.get('QUERY_RESULTS_PER_PAGE', 50)))
+        queries_filter = "published"
+    limit = int(request.args.get("limit", app.config.get("QUERY_RESULTS_PER_PAGE", 50)))
     queries, prev_link, next_link = QueriesRangeBasedPagination(
-        queries, request.args.get('from'), limit,
+        queries,
+        request.args.get("from"),
+        limit,
         request.path,
-        request.referrer, dict(request.args)).paginate()
+        request.referrer,
+        dict(request.args),
+    ).paginate()
     return render_template(
-        "query/list.html", user=get_user(), queries=queries,
-        prev_link=prev_link, next_link=next_link,
-        queries_filter=queries_filter)
+        "query/list.html",
+        user=get_user(),
+        queries=queries,
+        prev_link=prev_link,
+        next_link=next_link,
+        queries_filter=queries_filter,
+    )
 
 
-@app.route('/run/<int:qrun_id>/status')
+@app.route("/run/<int:qrun_id>/status")
 def run_status(qrun_id):
     qrun = g.conn.session.query(QueryRun).get(qrun_id)
     if not qrun:
-        return Response('No such query_run id', status=404)
-    return Response(json.dumps({
-        'status': qrun.status_message,
-        'extra': json.loads(qrun.extra_info or '{}'),
-        'timestamp': qrun.timestamp.strftime('%s')
-    }), mimetype='application/json', headers={'Access-Control-Allow-Origin': '*'})
+        return Response("No such query_run id", status=404)
+    return Response(
+        json.dumps(
+            {
+                "status": qrun.status_message,
+                "extra": json.loads(qrun.extra_info or "{}"),
+                "timestamp": qrun.timestamp.strftime("%s"),
+            }
+        ),
+        mimetype="application/json",
+        headers={"Access-Control-Allow-Origin": "*"},
+    )
 
 
 @app.route("/run/<int:qrun_id>/output/<int:resultset_id>/<string:format>")
-def output_result(qrun_id, resultset_id=0, format='json'):
+def output_result(qrun_id, resultset_id=0, format="json"):
     qrun = g.conn.session.query(QueryRun).get(qrun_id)
     if not qrun:
-        response = Response('No such query_run id', status=404)
+        response = Response("No such query_run id", status=404)
     else:
-        reader = SQLiteResultReader(qrun, app.config['OUTPUT_PATH_TEMPLATE'])
+        reader = SQLiteResultReader(qrun, app.config["OUTPUT_PATH_TEMPLATE"])
         try:
             response = output.get_formatted_response(format, qrun, reader, resultset_id)
         except sqlite3.OperationalError as e:
-            if e.args[0].startswith('no such table'):
-                response = Response('No such resultset id', status=404)
+            if e.args[0].startswith("no such table"):
+                response = Response("No such resultset id", status=404)
             else:
                 raise
-    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
 
@@ -359,15 +390,14 @@ def output_result(qrun_id, resultset_id=0, format='json'):
 def output_run_meta(qrun_id):
     qrun = g.conn.session.query(QueryRun).get(qrun_id)
     if not qrun:
-        return Response('No such query run id', status=404)
-    return Response(json.dumps(
-        {
-            'run': qrun,
-            'rev': qrun.rev,
-            'query': qrun.rev.query
-        }, default=json_formatter),
-        mimetype='application/json',
-        headers={'Access-Control-Allow-Origin': '*'},
+        return Response("No such query run id", status=404)
+    return Response(
+        json.dumps(
+            {"run": qrun, "rev": qrun.rev, "query": qrun.rev.query},
+            default=json_formatter,
+        ),
+        mimetype="application/json",
+        headers={"Access-Control-Allow-Origin": "*"},
     )
 
 
@@ -375,15 +405,14 @@ def output_run_meta(qrun_id):
 def output_rev_meta(rev_id):
     rev = g.conn.session.query(QueryRevision).get(rev_id)
     if not rev:
-        return Response('No such query revision id', status=404)
-    return Response(json.dumps(
-        {
-            'latest_run': rev.latest_run,
-            'rev': rev,
-            'query': rev.query
-        }, default=json_formatter),
-        mimetype='application/json',
-        headers={'Access-Control-Allow-Origin': '*'},
+        return Response("No such query revision id", status=404)
+    return Response(
+        json.dumps(
+            {"latest_run": rev.latest_run, "rev": rev, "query": rev.query},
+            default=json_formatter,
+        ),
+        mimetype="application/json",
+        headers={"Access-Control-Allow-Origin": "*"},
     )
 
 
@@ -391,15 +420,18 @@ def output_rev_meta(rev_id):
 def output_query_meta(query_id):
     query = g.conn.session.query(Query).get(query_id)
     if not query:
-        return Response('No such query id', status=404)
-    return Response(json.dumps(
-        {
-            'latest_run': query.latest_rev.latest_run,
-            'latest_rev': query.latest_rev,
-            'query': query
-        }, default=json_formatter),
-        mimetype='application/json',
-        headers={'Access-Control-Allow-Origin': '*'},
+        return Response("No such query id", status=404)
+    return Response(
+        json.dumps(
+            {
+                "latest_run": query.latest_rev.latest_run,
+                "latest_rev": query.latest_rev,
+                "query": query,
+            },
+            default=json_formatter,
+        ),
+        mimetype="application/json",
+        headers={"Access-Control-Allow-Origin": "*"},
     )
 
 
@@ -412,27 +444,30 @@ def output_query_meta(query_id):
 def output_explain(connection_id):
     cur = g.replica.connection.cursor()
     try:
-        cur.execute('SHOW EXPLAIN FOR %d;' % connection_id)
+        cur.execute("SHOW EXPLAIN FOR %d;" % connection_id)
     except cur.InternalError as e:
         if e.args[0] in [1094, 1915, 1933]:
             # 1094 = Unknown thread id
             # 1915, 1933 = Target is not running an EXPLAINable command
-            return Response(json.dumps(
-                {
-                    'headers': ['Error'],
-                    'rows': [['Hmm... Is the SQL actually running?!']],
-                }, default=json_formatter),
-                mimetype='application/json',
+            return Response(
+                json.dumps(
+                    {
+                        "headers": ["Error"],
+                        "rows": [["Hmm... Is the SQL actually running?!"]],
+                    },
+                    default=json_formatter,
+                ),
+                mimetype="application/json",
             )
         else:
             raise
     else:
-        return Response(json.dumps(
-            {
-                'headers': [c[0] for c in cur.description],
-                'rows': cur.fetchall(),
-            }, default=json_formatter),
-            mimetype='application/json',
+        return Response(
+            json.dumps(
+                {"headers": [c[0] for c in cur.description], "rows": cur.fetchall()},
+                default=json_formatter,
+            ),
+            mimetype="application/json",
         )
 
 
@@ -443,13 +478,12 @@ def pref_get(key):
 
     if key in get_preferences():
         return Response(
-            json.dumps({'key': key, 'value': get_preferences()[key]}),
-            mimetype='application/json'
+            json.dumps({"key": key, "value": get_preferences()[key]}),
+            mimetype="application/json",
         )
     else:
         return Response(
-            json.dumps({'key': key, 'error': 'novalue'}),
-            mimetype='application/json'
+            json.dumps({"key": key, "error": "novalue"}), mimetype="application/json"
         )
 
 
@@ -458,11 +492,11 @@ def pref_set(key, value):
     if get_user() is None:
         return "Authentication required", 401
 
-    get_preferences()[key] = (None if value == 'null' else value)
-    return Response(
-        json.dumps({'key': key, 'success': ''}),
-        mimetype='application/json'
-    ), 201
+    get_preferences()[key] = None if value == "null" else value
+    return (
+        Response(json.dumps({"key": key, "success": ""}), mimetype="application/json"),
+        201,
+    )
 
 
 if __name__ == '__main__':
@@ -475,7 +509,7 @@ def get_dbs():
     return Response(
         json.dumps(
             {
-                'dbs': list(
+                "dbs": list(
                     set(
                         db_result[-1].strip()
                         for db_result in known_dbs
@@ -486,9 +520,9 @@ def get_dbs():
                 )
             }
         ),
-        mimetype='application/json'
+        mimetype="application/json",
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5000, host="0.0.0.0")

9deb184bcb3f7b0b751f2e251d68df5dc24dde9b
Andrew Bogott <abogott@wikimedia.org>
2021-09-14 09:17:53 -0500

    Test api routes
    Change-Id: I558e20fb9b082e15a924f853f7c40919440f1789

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,7 @@ conf = {
     "OAUTH_SECRET_TOKEN": None,
     "REDIS_HOST": "localhost",
     "REDIS_PORT": 6379,
-    "REDIS_DB": None,
+    "REDIS_DB": 0,
     "DB_USER": "Quarry",
     "DB_PASSWORD": "no_such_password",
     "DB_HOST": "no_such_host",
@@ -22,6 +22,10 @@ def client(redisdb, mocker):
     # Make sure that we only ever use the redisdb function
     #  from pytest-redis
     mocker.patch("redis.Redis", return_value=redisdb)
+    mocker.patch(
+        "celery.backends.redis.RedisBackend.client",
+        new_callable=mocker.PropertyMock(return_value=redisdb),
+    )
 
     # kill_context is going to try to close a bunch of things
     #  that don't actually exist; just skip it.
